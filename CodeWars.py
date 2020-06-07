18576ebb671f6445ed4ed33cc65221957d1f94b7
Florent Berthet <florent.berthet@gmail.com>
2020-06-07 14:08:39 +0200

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -385,3 +385,165 @@ import string
 
 def is_pangram(s):
     return set(string.ascii_lowercase).issubset(s.lower())
+
+
+# Catching Car Mileage Numbers (4 kyu)
+
+'''
+"7777...8?!??!", exclaimed Bob, "I missed it again! Argh!" Every time there's an interesting number coming up, he notices and then promptly forgets. Who doesn't like catching those one-off interesting mileage numbers?
+
+Let's make it so Bob never misses another interesting number. We've hacked into his car's computer, and we have a box hooked up that reads mileage numbers. We've got a box glued to his dash that lights up yellow or green depending on whether it receives a 1 or a 2 (respectively).
+
+It's up to you, intrepid warrior, to glue the parts together. Write the function that parses the mileage number input, and returns a 2 if the number is "interesting" (see below), a 1 if an interesting number occurs within the next two miles, or a 0 if the number is not interesting.
+
+Note: In Haskell, we use No, Almost and Yes instead of 0, 1 and 2.
+
+"Interesting" Numbers
+Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
+
+Any digit followed by all zeros: 100, 90000
+Every digit is the same number: 1111
+The digits are sequential, incementing†: 1234
+The digits are sequential, decrementing‡: 4321
+The digits are a palindrome: 1221 or 73837
+The digits match one of the values in the awesome_phrases array
+† For incrementing sequences, 0 should come after 9, and not before 1, as in 7890. ‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
+'''
+
+def is_interesting(number, awesome_phrases):
+
+    if number < 98:
+        return 0
+
+    elif number == 98 or number == 99:
+        return 1
+
+# For inputs that must evaluate to 2
+
+    # Any digit followed by all zeros: 100, 90000
+    elif all(i == '0' for i in str(number)[1:]):
+        return 2
+
+
+    # Every digit is the same number: 1111
+    elif len(set(str(number))) == 1:
+        return 2
+
+
+    # The digits are sequential, incementing: 1234 (0 should come after 9, and not before 1, as in 7890)
+    elif str(number) in '1234567890':
+        return 2
+
+
+    # The digits are sequential, decrementing: 4321 (0 should come after 1, and not before 9, as in 3210)
+    elif str(number) in '9876543210':
+        return 2
+
+
+    # The digits are a palindrome: 1221 or 73837
+    elif number == int(''.join(reversed(str(number)))):
+        return 2
+
+
+    # The digits match one of the values in the awesome_phrases array
+    elif number in awesome_phrases:
+        return 2
+
+
+# For inputs that must evaluate to 1
+
+    # Any digit followed by all zeros: 100, 90000
+    elif all(i == str(number + 1)[1] for i in str(number + 1)[1:]):
+        return 1
+
+    elif all(i == str(number + 2)[1] for i in str(number + 2)[1:]):
+        return 1
+
+
+    # Every digit is the same number: 1111
+    elif len(set(str(number + 1))) == 1:
+        return 1
+
+    elif len(set(str(number + 2))) == 1:
+        return 1
+
+
+    # The digits are sequential, incementing: 1234 (0 should come after 9, and not before 1, as in 7890)
+    elif str(number + 1) in '1234567890':
+        return 1
+
+    elif str(number + 2) in '1234567890':
+        return 1
+
+
+    # The digits are sequential, decrementing: 4321 (0 should come after 1, and not before 9, as in 3210)
+    elif str(number + 1) in '9876543210':
+        return 1
+
+    elif str(number + 2) in '9876543210':
+        return 1
+
+
+    # The digits are a palindrome: 1221 or 73837
+    elif number + 1 == int(''.join(reversed(str(number + 1)))):
+        return 1
+
+    elif number + 2 == int(''.join(reversed(str(number + 2)))):
+        return 1
+
+
+    # The digits match one of the values in the awesome_phrases array
+    elif number + 1 in awesome_phrases:
+        return 1
+
+    elif number + 2 in awesome_phrases:
+        return 1
+
+    else:
+        return 0
+
+
+#  Solution 2
+
+def is_incrementing(number): return str(number) in '1234567890'
+def is_decrementing(number): return str(number) in '9876543210'
+def is_palindrome(number):   return str(number) == str(number)[::-1]
+def is_round(number):        return set(str(number)[1:]) == set('0')
+
+def is_interesting(number, awesome_phrases):
+    tests = (is_round, is_incrementing, is_decrementing,
+             is_palindrome, awesome_phrases.__contains__)
+       
+    for num, color in zip(range(number, number+3), (2, 1, 1)):
+        if num >= 100 and any(test(num) for test in tests):
+            return color
+    return 0
+
+
+#  Solution 3
+
+def is_interesting(number, awesome_phrases):
+    for i in [number, number+1, number+2]:
+        if i<100 :
+            continue
+        j=str(i)
+        if any([
+            i in awesome_phrases,
+            all([j[x]=='0' for x in range(1,len(j))]),
+            all([j[x]==j[0] for x in range(1,len(j))]),
+            j == j[::-1],
+            j in '1234567890',
+            j in '9876543210'
+                ]):
+            return 2-bool(number-i)
+    return 0
+
+#  Solution 4
+
+def is_interesting(number, awesome_phrases):
+    for r, num in zip((2, 1, 1), range(number, number + 3)):
+        num_str = str(num)
+        if num in awesome_phrases or num > 99 and (int(num_str[1:]) == 0 or num_str[::-1] == num_str or num_str in '1234567890' or num_str in '9876543210'):
+            return r
+    return 0
+
