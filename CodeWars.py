c98b795377ccf07a2c875e22f366b3f6fb3a7bac
Florent Berthet <florent.berthet@gmail.com>
2020-06-07 19:31:32 +0200

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -547,3 +547,66 @@ def is_interesting(number, awesome_phrases):
             return r
     return 0
 
+
+
+
+# Number of trailing zeros of N!
+
+'''
+Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+N! = 1 * 2 * 3 * ... * N
+
+Be careful 1000! has 2568 digits...
+
+For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+Examples
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros
+Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
+'''
+
+def zeros(n):
+    divisors=[]
+    div = 5
+    result = 0
+    if n >= 5:
+        while (div <= n):
+            divisors.append(div)
+            div = div*5
+        for i in divisors:
+            result = result + n//i
+    return result
+
+# Solution 2
+
+def zeros(n):
+  x = n//5
+  return x+zeros(x) if x else 0 
+
+# Solution 3
+
+def zeros(n):
+    zeros = 0
+    i = 5
+    while n//i > 0:
+        zeros += n//i
+        i*=5
+    return zeros
+
+# Solution 4
+
+def zeros(n):
+    result = 0
+    while n != 0:
+        result += n / 5
+        n = n / 5
+    return result
+
+# Solution 5
+def zeros(n):
+    return n/5 + zeros(n/5) if n >= 5 else 0
