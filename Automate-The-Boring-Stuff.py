020894287aba3e399a632eae74ca5f6450cd72d4
Florent Berthet <florent.berthet@gmail.com>
2020-06-29 21:03:51 +0200

    Update Automate-The-Boring-Stuff.py

--- Automate-The-Boring-Stuff.py
+++ Automate-The-Boring-Stuff.py
@@ -47,7 +47,8 @@ print('World')
 the output would look like this:
 Hello
 World
-The two strings appear on separate lines because the print() function automatically adds a newline character to the end of the string it is passed. However, you can set the end keyword argument to change this to a different string. For example, if the program were this:
+The two strings appear on separate lines because the print() function automatically adds a newline character to the end of the string it is passed. However, 
+you can set the end keyword argument to change this to a different string. For example, if the program were this:
 print('Hello', end='')
 print('World')
 the output would look like this:
@@ -58,10 +59,14 @@ HelloWorld”
 cats,dogs,mice”
 
 ---
-“Parameters and variables that are assigned in a called function are said to exist in that function’s local scope. Variables that are assigned outside all functions are said to exist in the global scope. A variable that exists in a local scope is called a local variable, while a variable that exists in the global scope is called a global variable. A variable must be one or the other; it cannot be both local and global.”
+“Parameters and variables that are assigned in a called function are said to exist in that function’s local scope. Variables that are assigned outside
+all functions are said to exist in the global scope. A variable that exists in a local scope is called a local variable,
+while a variable that exists in the global scope is called a global variable. A variable must be one or the other; it cannot be both local and global.”
 
 ---
-“Think of a scope as a container for variables. When a scope is destroyed, all the values stored in the scope’s variables are forgotten. There is only one global scope, and it is created when your program begins. When your program terminates, the global scope is destroyed, and all its variables are forgotten. Otherwise, the next time you ran your program, the variables would remember their values from the last time you ran it.”
+“Think of a scope as a container for variables. When a scope is destroyed, all the values stored in the scope’s variables are forgotten.
+There is only one global scope, and it is created when your program begins. When your program terminates, the global scope is destroyed,
+and all its variables are forgotten. Otherwise, the next time you ran your program, the variables would remember their values from the last time you ran it.”
 
 ---
 
@@ -69,17 +74,22 @@ cats,dogs,mice”
 Code in the global scope cannot use any local variables.
 However, a local scope can access global variables.
 Code in a function’s local scope cannot use variables in any other local scope.
-You can use the same name for different variables if they are in different scopes. That is, there can be a local variable named spam and a global variable also named spam.”
+You can use the same name for different variables if they are in different scopes. That is, there can be a local variable named spam and a global
+variable also named spam.”
 ---
 
-“The reason Python has different scopes instead of just making everything a global variable is so that when variables are modified by the code in a particular call to a function, the function interacts with the rest of the program only through its parameters and the return value. This narrows down the list code lines that may be causing a bug.”
+“The reason Python has different scopes instead of just making everything a global variable is so that when variables are modified by the code in
+a particular call to a function, the function interacts with the rest of the program only through its parameters and the return value.
+This narrows down the list code lines that may be causing a bug.”
 
 ---
 “While using global variables in small programs is fine, it is a bad habit to rely on global variables as your programs get larger and larger.”
 
 ---
 “The global Statement
-If you need to modify a global variable from within a function, use the global statement. If you have a line such as global eggs at the top of a function, it tells Python, “In this function, eggs refers to the global variable, so don’t create a local variable with this name.” For example, type the following code into the file editor and save it as sameName2.py:
+If you need to modify a global variable from within a function, use the global statement. If you have a line such as global eggs at the top of a function,
+it tells Python, “In this function, eggs refers to the global variable, so don’t create a local variable with this name.” For example, type the following
+code into the file editor and save it as sameName2.py:
   def spam():
 ➊    global eggs
 ➋    eggs = 'spam'
@@ -98,7 +108,8 @@ Otherwise, if the variable is used in an assignment statement in the function, i
 But if the variable is not used in an assignment statement, it is a global variable.”
 
 ---
-“Errors can be handled with try and except statements. The code that could potentially have an error is put in a try clause. The program execution moves to the start of a following except clause if an error happens.”
+“Errors can be handled with try and except statements. The code that could potentially have an error is put in a try clause.
+The program execution moves to the start of a following except clause if an error happens.”
 
 ---
 “def spam(divideBy):
@@ -112,13 +123,15 @@ print(spam(2))
 print(spam(12))
 print(spam(0))
 print(spam(1))
-When code in a try clause causes an error, the program execution immediately moves to the code in the except clause. After running that code, the execution continues as normal. The output of the previous program is as follows:
+When code in a try clause causes an error, the program execution immediately moves to the code in the except clause.
+After running that code, the execution continues as normal. The output of the previous program is as follows:
 21.0
 3.5
 Error: Invalid argument.
 None
 42.0
-Note that any errors that occur in function calls in a try block will also be caught. Consider the following program, which instead has the spam() calls in the try block:
+Note that any errors that occur in function calls in a try block will also be caught. Consider the following program, which instead has the spam()
+calls in the try block:
 def spam(divideBy):
     return 42 / divideBy
 
@@ -133,7 +146,8 @@ When this program is run, the output looks like this:
 21.0
 3.5
 Error: Invalid argument.
-The reason print(spam(1)) is never executed is because once the execution jumps to the code in the except clause, it does not return to the try clause. Instead, it just continues moving down as normal.”
+The reason print(spam(1)) is never executed is because once the execution jumps to the code in the except clause, it does not return to the try clause. 
+Instead, it just continues moving down as normal.”
 
 ---
 “Lists can also contain other list values. The values in these lists of lists can be accessed using multiple indexes,
@@ -262,7 +276,8 @@ And, Or, Not
 “>>> type(('hello',))
 <class 'tuple'>”
 
-“However, lists can be modified because they are mutable objects. The append() method doesn’t create a new list object; it changes the existing list object. We call this “modifying the object in-place.”
+“However, lists can be modified because they are mutable objects. The append() method doesn’t create a new list object; it changes the existing list object.
+We call this “modifying the object in-place.”
 
 >>> eggs.append('moose') # append() modifies the list "in place".
 >>> eggs = ['bat', 'rat', 'cow'] # This creates a new list, which has a new
